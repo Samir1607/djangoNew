a49688df1e360105f230767c2508e799dcaa6ace
Samir1607 <samirpadekar99@gmail.com>
2023-09-21 10:15:03 +0530

    adding models and views to it

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.CharField(max_length=3)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " is from " + self.city +"."
--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Students
+from .serializers import StudentsSerializer  # You need to create a serializer for your model
 
-# Create your views here.
+
+class StudentsViewSet(viewsets.ModelViewSet):
+    queryset = Students.objects.all()
+    serializer_class = StudentsSerializer
